ee60be64d35511d78cf4d42812d64ae0973fb73c
Rubenespza <70655396+Rubenespza@users.noreply.github.com>
2021-03-24 22:53:24 -0600

    Add files via upload

---
+++ Practica23/Determinar.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica23
+{
+    class Determinar
+    {
+        public string numayor (int a, int b, int c, int d, int e)
+        {
+            string resultado = "";
+            if ((a > b ) && (a > c) && (a > d) && (a > e))
+            {
+                resultado = "El" + a + "es el mayor";
+            }
+            if ((b > a) && (b > c) && (b> d) && (b > e))
+            {
+                resultado = "El" + b + "es el mayor";
+            }
+            if ((c > a) && (c > b) && (c > d) && (c > e))
+            {
+                resultado = "El" + c + "es el mayor";
+            }
+            if ((d > a) && (d > b) && (d > c) && (a > e))
+            {
+                resultado = "El" + d + "es el mayor";
+            }
+            if ((e > a) && (e > b) && (e > c) && (e > d))
+            {
+                resultado = "El" + e + "es el mayor";
+            }
+            return resultado;
+        }
+    }
+}
---
+++ Practica23/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Practica23
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Determinar x = new Determinar();
+            int n1 = int.Parse(textBox1.Text);
+            int n2 = int.Parse(textBox2.Text);
+            int n3 = int.Parse(textBox3.Text);
+            int n4 = int.Parse(textBox4.Text);
+            int n5 = int.Parse(textBox5.Text);
+            string cadena = x.numayor(n1, n2, n3, n4, n5);
+            MessageBox.Show(cadena, "Numero Mayor", MessageBoxButtons.OK, MessageBoxIcon.None);
+
+        }
+    }
+}
